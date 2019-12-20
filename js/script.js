5813c427ac1f7505e2f15b22f0d392c3be71cff7
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-20 14:52:23 +0300

    sred

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-var num = [3, 4, 5, 5, 5];
+var num = [5, 4, 5, 5, 5];
 summa = 0;
 sredne = 0;
+
 for (var i = 0; i < num.length; i++) {
     summa += num[i];
     sredne = summa / num.length;
 }
-console.log(summa);
+
 console.log(sredne);
